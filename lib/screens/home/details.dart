593aef4ff685c030aa17cdc15cbdaafc4cc0d5b1
weslyyy <weslydeandrademoraes@gmail.com>
2021-08-01 23:47:08 -0300

    terceito commit

--- lib/screens/home/details.dart
+++ lib/screens/home/details.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tudo_gostoso/models/recipe.dart';
 import 'package:tudo_gostoso/style.dart';
 
 class Details extends StatelessWidget {
+  final Recipe _recipeObj;
+
+  Details(this._recipeObj);
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -14,13 +19,14 @@ class Details extends StatelessWidget {
       child: Column(
         children: [
           Text(
-            "Bolo Gelado",
+            _recipeObj.name,
             style: TextStyle(
               color: Colors.white,
               fontSize: 34,
             ),
           ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               // 1° Coluna
               Column(
@@ -29,23 +35,110 @@ class Details extends StatelessWidget {
                     FontAwesomeIcons.solidClock,
                     color: Colors.white,
                   ),
-                  Text(
-                    "PREPARO",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                    ),
+                    child: Text(
+                      "PREPARO",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                   Text(
-                    "40 MIN",
+                    "${_recipeObj.preparationTime} MIN",
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 12,
                     ),
                   ),
                 ],
-              )
+              ),
+              // 2° Coluna
+              Column(
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.cheese,
+                    color: Colors.white,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                    ),
+                    child: Text(
+                      "RENDIMENTO",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "12 PORÇÕES",
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+              // 3° Coluna
+              Column(
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.solidHeart,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    "FAVORITOS",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 12,
+                    ),
+                  ),
+                  Text(
+                    "332936",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+              // 4° Coluna
+              Column(
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.solidCommentAlt,
+                    color: Colors.white,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                    ),
+                    child: Text(
+                      "COMENTÁRIOS",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "6847",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ],
